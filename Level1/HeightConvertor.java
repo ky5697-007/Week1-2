c27d491ec486fee3557b152ca9397b5cfc649258
ky5697 -007 <ky5697@srmist.edu.in>
2026-02-21 12:37:07 +0530

    Koushik added HeightConvertor to git

---
+++ Level1/HeightConvertor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HeightConvertor {
+    public static void main(String[] args) {
+        double heightCm;
+        double totalInches;
+        int feet;
+        double inches;
+        Scanner input = new Scanner(System.in);
+        heightCm = input.nextDouble();
+        totalInches = heightCm / 2.54;
+        feet = (int) (totalInches / 12);
+        inches = totalInches % 12;
+
+        System.out.println(
+                "Your Height in cm is " + heightCm +
+                " while in feet is " + feet +
+                " and inches is " + inches
+        );
+
+        input.close();
+    }
+}
