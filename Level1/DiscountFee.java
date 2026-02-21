d44b9c21d993260894f086719476bc72a0015b57
ky5697 -007 <ky5697@srmist.edu.in>
2026-02-21 12:32:07 +0530

    Koushik added DiscoutFee to git

---
+++ Level1/DiscountFee.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DiscountFee {
+    public static void main(String[] args) {
+        double fee;
+        double discountPercent;
+
+        Scanner input = new Scanner(System.in);
+        fee = input.nextDouble();
+        discountPercent = input.nextDouble();
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+
+        System.out.println( "The discount amount is INR " + discount +" and final discounted fee is INR " + finalFee);
+
+        input.close();
+    }
+}
