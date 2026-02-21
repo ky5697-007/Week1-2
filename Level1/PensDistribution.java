00ce4c9416934a27f6e2b3a36b636751f88c6879
ky5697 -007 <ky5697@srmist.edu.in>
2026-02-21 12:06:08 +0530

    Koushik added CourseFeeDiscount to git

--- Level1/PensDistribution.java
+++ Level1/PensDistribution.java
@@ -1,14 +1,14 @@
 public class PensDistribution {
-        public static void main(String[] args) {
-        double costPrice = 129;
-        double sellingPrice = 191;
+     public static void main(String[] args) {
+        int totalPens = 14;
+        int students = 3;
 
-        double profit = sellingPrice - costPrice;
-        double profitPercentage = (profit / costPrice) * 100;
+        int pensPerStudent = totalPens / students;   
+        int remainingPens = totalPens % students;    
 
         System.out.println(
-                "The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice + "\n" +
-                "The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage
+                "The Pen Per Student is " + pensPerStudent +
+                " and the remaining pen not distributed is " + remainingPens
         );
     }
 
